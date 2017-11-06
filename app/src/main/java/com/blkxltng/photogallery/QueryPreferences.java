e5fb4010f833c16cf78c858e87e5182635f35ad2
blkxltng <firejama@gmail.com>
2017-11-06 18:45:14 -0500

    Added SearchView to allow user to search Flickr

---
+++ app/src/main/java/com/blkxltng/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package com.blkxltng.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by firej on 11/6/2017.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
