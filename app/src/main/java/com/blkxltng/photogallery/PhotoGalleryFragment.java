e9f3bbf7ba6d27dce3bbf510409dce34e3113cc1
blkxltng <firejama@gmail.com>
2017-11-13 20:09:33 -0500

    Notification now only shows if you're not in the app

--- app/src/main/java/com/blkxltng/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/blkxltng/photogallery/PhotoGalleryFragment.java
@@ -7,7 +7,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -27,7 +26,7 @@ import java.util.List;
  * Created by firej on 10/24/2017.
  */
 
-public class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends VisibleFragment {
 
     private static final String TAG = "PhotoGalleryFragment";
 
